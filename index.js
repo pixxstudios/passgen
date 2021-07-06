b92a6189b019a98fd949b07a0b6886e1982cfe1b
Gagandeep Singh <gagan.ddun@gmail.com>
2021-07-06 22:36:07 +1000

    help options added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const program = require('commander');
 
-program.version('1.0.0').description('Password Generator').parse();
+program.version('1.0.0').description('Password Generator');
+
+program
+  .option('-l, --length <number>', 'length of password', '8')
+  .option('-s, --save', 'save password to password.txt')
+  .option('-nn, --no-numbers', 'remove numbers')
+  .parse();
+
+console.log(program.opts());
