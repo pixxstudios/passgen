3de38a663e8b704b367f6d3f6db8b3c62f7b6b3c
Gagandeep Singh <gagan.ddun@gmail.com>
2021-07-07 20:22:10 +1000

    generate random password

--- utils/password-generator.js
+++ utils/password-generator.js
@@ -2,9 +2,17 @@ const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const allNumbers = '0123456789';
 const allSymbols = '!@#$%^&*_-+=';
 const generatePassword = ({ length, save, numbers, symbols }) => {
-  let password = alpha;
-  numbers ? (password += allNumbers) : '';
-  symbols ? (password += allSymbols) : '';
+  let chars = alpha;
+  numbers ? (chars += allNumbers) : '';
+  symbols ? (chars += allSymbols) : '';
+  //   return password;
+
+  let password = '';
+
+  for (let i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
   return password;
 };
 
